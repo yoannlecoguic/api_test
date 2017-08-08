6e91f8e18f26f7cdf4d499238319c567bf7f75a1
yoannlecoguic <yoann.lecoguic@gmail.com>
2017-08-09 00:45:51 +0200

    add jsdoc

--- controllers/parkingController.js
+++ controllers/parkingController.js
@@ -1,21 +1,30 @@
 'use strict';
 
+/** get spot by id */
 exports.getSpot = function(req, res) {
 	res.send("get spot");
 };
 
+/** add spot by id */
 exports.addSpot = function(req, res) {
 	res.send("add spot");
 };
 
+/** update spot by id */
 exports.updateSpot = function(req, res) {
 	res.send("update spot");
 };
 
+/** delete spot by id */
 exports.deleteSpot = function(req, res) {
 	res.send("delete spot");
 };
 
+/**
+*	search spot using :
+*	@param {int=|null} userId - Is the spot is free and by whom
+*	@param {int=|null} floor - Search a spot by floor
+*/
 exports.search = function(req, res) {
 	res.send("search spot");
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,12 @@
 var Jwt = require('jsonwebtoken');
 var jwtOptions = require('../jwtOptions.json');
 
+/**
+*	Post credentials et get token if credentials matches
+*	@param {string} username - user username
+*	@param {string} password - user password
+*/
 exports.login = function(req, res) {
-	// Task.find({}, function(err, task) {
-	// 	if (err)
-	// 		res.send(err);
-
-	// 	res.json(task);
-	// });
-
 	var payload = {id: req.body.id};
 	var token = Jwt.sign(payload, jwtOptions.privateKey);
 
@@ -20,18 +18,27 @@ exports.login = function(req, res) {
 	});
 };
 
+/**
+*	Register new user
+*	@param {string} username - user username
+*	@param {string} password - user password
+*	@param {string} type - ROLE_ADMIN or ROLE_PUBLIC
+*/
 exports.register = function(req, res) {
 	res.send("register");
 };
 
+/** Get user by id */
 exports.getUser = function(req, res) {
 	res.send("get user");
 };
 
+/** Update user by id */
 exports.updateUser = function(req, res) {
 	res.send("update user");
 };
 
+/** Delete user by id */
 exports.deleteUser = function(req, res) {
 	res.send("delete user");
 };
